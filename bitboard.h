cad9830a9b7e12c3069e45fef374cad6d21a1dc9
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-09-21 18:23:23 -0700

    Adds a build rule for the bitboard

---
+++ bitboard.h
@@ -0,0 +1,13 @@
+#ifndef CHESS_ENGINE_BITBOARD_H_
+#define CHESS_ENGINE_BITBOARD_H_
+
+#include <cstdint>
+
+namespace chessengine::bitboard {
+
+using Bitboard = std::uint64_t;
+
+} // namespace chessengine::bitboard
+
+#endif // CHESS_ENGINE_BITBOARD_H_
+
