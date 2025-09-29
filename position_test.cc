70b80ca785ce5300370756e86d596a406d8b13d3
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-09-28 22:39:35 -0700

    Adds more FEN tests

--- position_test.cc
+++ position_test.cc
@@ -11,7 +11,7 @@ using ::testing::Eq;
 using ::testing::IsFalse;
 using ::testing::IsTrue;
 
-TEST(FEN, Default) {
+TEST(FEN, Initial) {
     std::expected<Position, std::string> position = Position::Make(
             "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
@@ -25,6 +25,54 @@ TEST(FEN, Default) {
                     "3: . . . . . . . .\n"
                     "2: P P P P P P P P\n"
                     "1: R N B Q K B N R\n"
+                    "   a b c d e f g h\n"
+                    "\n"
+                    "      Turn: -\n"
+                    "  Castling: -\n"
+                    "En Passant: -\n"
+                    "Half Moves: 0\n"
+                    "Full Moves: 0\n"
+                ));
+}
+
+TEST(FEN, Kiwipete) {
+    std::expected<Position, std::string> position = Position::Make(
+            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+
+    ASSERT_THAT(position.has_value(), IsTrue()) << position.error();
+    EXPECT_THAT(std::format("{}", position.value()), Eq(
+                    "8: r . . . k . . r\n"
+                    "7: p . p p q p b .\n"
+                    "6: b n . . p n p .\n"
+                    "5: . . . P N . . .\n"
+                    "4: . p . . P . . .\n"
+                    "3: . . N . . Q . p\n"
+                    "2: P P P B B P P P\n"
+                    "1: R . . . K . . R\n"
+                    "   a b c d e f g h\n"
+                    "\n"
+                    "      Turn: -\n"
+                    "  Castling: -\n"
+                    "En Passant: -\n"
+                    "Half Moves: 0\n"
+                    "Full Moves: 0\n"
+                ));
+}
+
+TEST(FEN, Sparse) {
+    std::expected<Position, std::string> position = Position::Make(
+            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
+
+    ASSERT_THAT(position.has_value(), IsTrue()) << position.error();
+    EXPECT_THAT(std::format("{}", position.value()), Eq(
+                    "8: . . . . . . . .\n"
+                    "7: . . p . . . . .\n"
+                    "6: . . . p . . . .\n"
+                    "5: K P . . . . . r\n"
+                    "4: . R . . . p . k\n"
+                    "3: . . . . . . . .\n"
+                    "2: . . . . P . P .\n"
+                    "1: . . . . . . . .\n"
                     "   a b c d e f g h\n"
                     "\n"
                     "      Turn: -\n"
