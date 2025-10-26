e0fc45fc2a70d5fae5b069d15322f5c5d55b79c3
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-10-26 16:42:57 -0700

    Adds more perft tests

--- perft_test.cc
+++ perft_test.cc
@@ -73,12 +73,33 @@ INSTANTIATE_TEST_SUITE_P(
 
             // https://www.chessprogramming.org/Perft_Results#Position_3
             PerftTestCase{
-            .name = "position3",
+            .name = "Position3",
             .fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
 
             // TODO(aryann): This is wrong. Fix this once the move generator can
             // correctly generate all moves.
             .expected_node_counts = {1, 14, 191, 2'808},
+            },
+
+            // https://www.chessprogramming.org/Perft_Results#Position_4
+            PerftTestCase{
+            .name = "Position4",
+            .fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+            .expected_node_counts = {1, 6, 264},
+            },
+
+            // https://www.chessprogramming.org/Perft_Results#Position_5
+            PerftTestCase{
+            .name = "Position5",
+            .fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            .expected_node_counts = {1, 44},
+            },
+
+            // https://www.chessprogramming.org/Perft_Results#Position_6
+            PerftTestCase{
+            .name = "Position6",
+            .fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            .expected_node_counts = {1, 46},
             }
         ),
         GetTestName);
