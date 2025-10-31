4c971dbeb35594592f0685c4b73259dde8ffd18f
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-10-31 16:04:18 -0700

    Divides the perft code to easily allow for multiple threads

--- perft.cc
+++ perft.cc
@@ -3,27 +3,26 @@
 #include <map>
 #include <vector>
 
+#include "absl/log/log.h"
 #include "move.h"
 #include "move_generator.h"
 #include "position.h"
 #include "scoped_move.h"
 
 namespace chessengine {
-
 namespace {
 
-void RunPerft(std::size_t depth, std::size_t current_depth, Position &position,
-              std::optional<Move> start_move,
-              std::vector<std::size_t> &depth_counts,
-              std::map<Move, std::size_t> &final_move_counts) {
+std::size_t RunPerft(std::size_t depth, std::size_t current_depth,
+                     Position &position, Move start_move,
+                     std::vector<std::size_t> &depth_counts) {
   ++depth_counts[current_depth];
 
   if (depth == current_depth) {
-    ++final_move_counts[*start_move];
-    return;
+    return 1;
   }
 
   std::vector<Move> moves = GenerateMoves(position);
+  std::size_t final_move_count = 0;
 
   for (const Move &move : moves) {
     ScopedMove scoped_move(move, position);
@@ -31,10 +30,11 @@ void RunPerft(std::size_t depth, std::size_t current_depth, Position &position,
     if (position.GetCheckers(~position.SideToMove())) {
       continue;
     }
-
-    RunPerft(depth, current_depth + 1, position, start_move ? start_move : move,
-             depth_counts, final_move_counts);
+    final_move_count +=
+        RunPerft(depth, current_depth + 1, position, start_move, depth_counts);
   }
+
+  return final_move_count;
 }
 
 }  // namespace
@@ -43,8 +43,22 @@ void RunPerft(std::size_t depth, Position &position,
               std::vector<std::size_t> &depth_counts,
               std::map<Move, std::size_t> &final_move_counts) {
   depth_counts.resize(depth + 1, 0);
+  depth_counts[0] = 1;
+  if (depth == 0) {
+    return;
+  }
 
-  RunPerft(depth, 0, position, std::nullopt, depth_counts, final_move_counts);
+  std::vector<Move> initial_moves = GenerateMoves(position);
+
+  for (const Move move : initial_moves) {
+    Position new_position = position;
+    new_position.Do(move);
+    if (new_position.GetCheckers(~new_position.SideToMove())) {
+      continue;
+    }
+    final_move_counts[move] =
+        RunPerft(depth, 1, new_position, move, depth_counts);
+  }
 }
 
 }  // namespace chessengine
