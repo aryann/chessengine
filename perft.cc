f1230694049d8f973acc03fa7e119b1f2dbfc5fa
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-10-31 16:11:27 -0700

    Creates independent depth count tracking for the perft function

--- perft.cc
+++ perft.cc
@@ -40,24 +40,35 @@ std::size_t RunPerft(std::size_t depth, std::size_t current_depth,
 }  // namespace
 
 void RunPerft(std::size_t depth, Position &position,
-              std::vector<std::size_t> &depth_counts,
+              std::vector<std::size_t> &final_depth_counts,
               std::map<Move, std::size_t> &final_move_counts) {
-  depth_counts.resize(depth + 1, 0);
-  depth_counts[0] = 1;
   if (depth == 0) {
     return;
   }
 
   std::vector<Move> initial_moves = GenerateMoves(position);
+  std::vector<std::vector<std::size_t>> all_depth_counts(
+      initial_moves.size(), std::vector<std::size_t>(depth + 1, 0));
 
-  for (const Move move : initial_moves) {
+  for (int i = 0; i < initial_moves.size(); ++i) {
+    Move move = initial_moves[i];
     Position new_position = position;
     new_position.Do(move);
     if (new_position.GetCheckers(~new_position.SideToMove())) {
       continue;
     }
+
+    std::vector<std::size_t> depth_counts(depth + 1, 0);
     final_move_counts[move] =
-        RunPerft(depth, 1, new_position, move, depth_counts);
+        RunPerft(depth, 1, new_position, move, all_depth_counts[i]);
+  }
+
+  final_depth_counts.resize(depth + 1, 0);
+  final_depth_counts[0] = 1;
+  for (const std::vector<std::size_t> &depth_counts : all_depth_counts) {
+    for (int i = 0; i < depth_counts.size(); ++i) {
+      final_depth_counts[i] += depth_counts[i];
+    }
   }
 }
 
--- perft.h
+++ perft.h
@@ -9,7 +9,7 @@
 namespace chessengine {
 
 void RunPerft(std::size_t depth, Position &position,
-              std::vector<std::size_t> &depth_counts,
+              std::vector<std::size_t> &final_depth_counts,
               std::map<Move, std::size_t> &final_move_counts);
 
 }  // namespace chessengine
